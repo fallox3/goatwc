309d75e883d101b6ea838c0a442579cad5d1e3c0
NOWY <82639144+NOWY2115@users.noreply.github.com>
2023-03-31 18:26:33 +0200

    Optymalizacja wyświetlania
    Dodane wyświetlanie różnych końcówek dat w zależności od wartości

--- script.js
+++ script.js
@@ -5,32 +5,50 @@ function f1(){
     let sec = (theday, today) =>{
         let time = today.getTime() - theday.getTime();
         let total = Math.ceil(time / (1000));
-        return total;
+        if (total == 1)
+            return total + " sekunda";
+        if (total > 1 && total < 5)
+            return total + " sekundy";
+        return total + " sekund";
     }
 
     let min = (theday, today) =>{
         let time = today.getTime() - theday.getTime();
         let total = Math.ceil(time / (1000 * 60));
-        return total;
+        if (total == 1)
+            return total + " minuta";
+        if (total > 1 && total < 5)
+            return total + " minuty";
+        return total + " minut";
     }
 
     let hour = (theday, today) =>{
         let time = today.getTime() - theday.getTime();
         let total = Math.ceil(time / (1000 * 60 * 60));
-        return total;
+        if (total == 1)
+            return total + " godzina";
+        if (total > 1 && total < 5)
+            return total + " godziny";
+        return total + " godzin";
     }
 
     let day = (theday, today) =>{
         let time = today.getTime() - theday.getTime();
         let total = Math.ceil(time / (1000 * 60 * 24 * 60));
-        return total;
+        if (total == 1)
+            return total + " dzień";
+        return total + " dni";
     }
     
     let month = (theday, today) =>{
         let total = (today.getFullYear() - theday.getFullYear()) * 12;
         total -= theday.getMonth();
         total += today.getMonth();
-        return total;
+        if (total == 1)
+            return total + " miesiąc";
+        if (total > 1 && total < 5)
+            return total + " miesiące";
+        return total + " miesięcy";
     }
     
     let year = (theday, today) =>{
@@ -38,15 +56,19 @@ function f1(){
         time -= theday.getMonth();
         time += today.getMonth();
         let total = Math.floor(time / 12);
-        return total;
+        if (total == 1)
+            return total + " rok";
+        if (total > 1 && total < 5)
+            return total + " lata";
+        return total + " lat";
     }
 
-    document.querySelector("#sec").innerHTML = sec(theday, today) + " sekund";
-    document.querySelector("#min").innerHTML = min(theday, today) + " minut";
-    document.querySelector("#hour").innerHTML = hour(theday, today) + " godzin";
-    document.querySelector("#day").innerHTML = day(theday, today) + " dni";
-    document.querySelector("#month").innerHTML = month(theday, today) + " miesięcy";
-    document.querySelector("#year").innerHTML = year(theday, today) + " lat";
+    document.querySelector("#sec").innerHTML = sec(theday, today);
+    document.querySelector("#min").innerHTML = min(theday, today);
+    document.querySelector("#hour").innerHTML = hour(theday, today);
+    document.querySelector("#day").innerHTML = day(theday, today);
+    document.querySelector("#month").innerHTML = month(theday, today);
+    document.querySelector("#year").innerHTML = year(theday, today);
 }
 f1();
 window.setInterval("f1()", 1000);
