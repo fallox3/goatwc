cc6091ae169c3338ad660d5ff14894a5a1b88020
NOWY <82639144+NOWY2115@users.noreply.github.com>
2023-03-31 17:31:33 +0200

    Update skryptu
    Dodanie funkcjonalności miesiąca i roku do kodu JS oraz dostosowanie HTML pod nowe wyświetlanie

--- script.js
+++ script.js
@@ -25,11 +25,29 @@ function f1(){
         let total = Math.ceil(time / (1000 * 60 * 24 * 60));
         return total;
     }
+    
+    let month = (theday, today) =>{
+        //let time = today.getTime() - theday.getTime();
+        let total = (today.getFullYear() - theday.getFullYear()) * 12;
+        total -= theday.getMonth();
+        total += today.getMonth();
+        return total;
+    }
+    
+    let year = (theday, today) =>{
+        let time = (today.getFullYear() - theday.getFullYear()) * 12;
+        time -= theday.getMonth();
+        time += today.getMonth();
+        let total = Math.ceil(time / 12);
+        return total;
+    }
 
     document.querySelector("#sec").innerHTML = sec(theday, today) + " sekund";
     document.querySelector("#min").innerHTML = min(theday, today) + " minut";
     document.querySelector("#hour").innerHTML = hour(theday, today) + " godzin";
     document.querySelector("#day").innerHTML = day(theday, today) + " dni";
+    document.querySelector("#month").innerHTML = day(theday, today) + " miesięcy";
+    document.querySelector("#year").innerHTML = day(theday, today) + " lat";
 }
 f1();
 window.setInterval("f1()", 1000);
