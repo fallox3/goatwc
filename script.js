a6f63339a9bf9c062b78993dc298cf417b0d018c
NOWY2115 <82639144+NOWY2115@users.noreply.github.com>
2023-03-31 09:30:36 +0200

    Optymalizacja algorytmu
    Usunięcie niepotrzebnych linii kodu, zastosowanie indentacji dla większej czytelności

--- script.js
+++ script.js
@@ -2,35 +2,34 @@ function f1(){
     let theday = new Date('12/18/2022 21:41:00');
     let today = new Date();
 
-let sec = (theday, today) =>{
-    let time = today.getTime() - theday.getTime();
-    let total = Math.ceil(time / (1000));
-    return total;
-}
+    let sec = (theday, today) =>{
+        let time = today.getTime() - theday.getTime();
+        let total = Math.ceil(time / (1000));
+        return total;
+    }
 
-let min = (theday, today) =>{
-    let time = today.getTime() - theday.getTime();
-    let total = Math.ceil(time / (1000 * 60));
-    return total;
-}
+    let min = (theday, today) =>{
+        let time = today.getTime() - theday.getTime();
+        let total = Math.ceil(time / (1000 * 60));
+        return total;
+    }
 
-let hour = (theday, today) =>{
-    let time = today.getTime() - theday.getTime();
-    let total = Math.ceil(time / (1000 * 60 * 60));
-    return total;
-}
+    let hour = (theday, today) =>{
+        let time = today.getTime() - theday.getTime();
+        let total = Math.ceil(time / (1000 * 60 * 60));
+        return total;
+    }
 
-let day = (theday, today) =>{
-    let time = today.getTime() - theday.getTime();
-    let total = Math.ceil(time / (1000 * 60 * 24 * 60));
-    return total;
-}
-console.log(sec(theday, today));
-document.querySelector("#sec").innerHTML = "sekund: " + sec(theday, today);
-document.querySelector("#min").innerHTML = "minut: " + min(theday, today);
-document.querySelector("#hour").innerHTML = "godzin: " + hour(theday, today);
-document.querySelector("#day").innerHTML = "dni: " + day(theday, today);
-//document.querySelector("#sec").innerHTML = sec(theday, today) + " " + min(theday, today) + "    " + hour(theday, today) + " " +  day(theday, today);
+    let day = (theday, today) =>{
+        let time = today.getTime() - theday.getTime();
+        let total = Math.ceil(time / (1000 * 60 * 24 * 60));
+        return total;
+    }
+
+    document.querySelector("#sec").innerHTML = sec(theday, today) + " sekund";
+    document.querySelector("#min").innerHTML = min(theday, today) + " minut";
+    document.querySelector("#hour").innerHTML = hour(theday, today) + " godzin";
+    document.querySelector("#day").innerHTML = day(theday, today) + " dni";
 }
 f1();
 window.setInterval("f1()", 1000);
